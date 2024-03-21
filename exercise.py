7ed8f136530113834d0a71fe47ff2674e6967471
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-03-21 08:52:23 +0300

    done the exercise.py coode

---
+++ exercise.py
@@ -0,0 +1,5 @@
+class Exercise:
+    def __init__(self, name, targeted_muscle_groups, calories_burned_per_minute):
+        self.name = name
+        self.targeted_muscle_groups = targeted_muscle_groups
+        self.calories_burned_per_minute = calories_burned_per_minute
